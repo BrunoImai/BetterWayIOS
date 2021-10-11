a083e11a4eacaa1d926fcf7cf7101dbc44111c26
BrunoImai <bruno.y.l.imai@gmail.com>
2021-10-11 10:42:51 -0300

    Initial Commit

---
+++ BetterWay/BetterWay.h
@@ -0,0 +1,18 @@
+//
+//  BetterWay.h
+//  BetterWay
+//
+//  Created by Bruno Imai on 11/10/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BetterWay.
+FOUNDATION_EXPORT double BetterWayVersionNumber;
+
+//! Project version string for BetterWay.
+FOUNDATION_EXPORT const unsigned char BetterWayVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BetterWay/PublicHeader.h>
+
+
